3f255712c3c64f9e371abf8ec0cad3a196506668
allanchristiang <allancgadelha@gmail.com>
2021-11-15 15:29:51 -0300

    Api server POC

--- server/src/controllers/filmes/index.js
+++ server/src/controllers/filmes/index.js
@@ -1,2 +1,19 @@
+'use restrict'
 
+const express = require('express')
+const app = express()
+
+app.get('/', function(req, res) {
+    res.send('hello world')
+})
+
+app.post('/', function(req, res) {
+    res.send("Hello, I am a post req")
+})
+
+app.put('/', function(req, res) {
+    res.send("Hello, I am update something")
+})
+
+app.listen(8000, () => console.log("Listening on port 8000"))
 
